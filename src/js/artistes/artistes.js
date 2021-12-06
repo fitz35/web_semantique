b58f7f3be54dd60c1a93d5c752345f9f16531ae6
Tushita Ramkarn <tushitaramkaran@gmail.com>
2021-12-06 11:12:30 +0100

    ajouter awards

--- src/js/artistes/artistes.js
+++ src/js/artistes/artistes.js
@@ -91,6 +91,44 @@ function getArtistSongs(name){
 };
 
 
+function getArtistAwards(name){
+    var contenu_requete =     
+    `PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+     PREFIX dc: <http://purl.org/dc/elements/1.1/>
+     PREFIX : <http://dbpedia.org/resource/>
+     PREFIX dbpedia2: <http://dbpedia.org/property/>
+     PREFIX dbpedia: <http://dbpedia.org/>
+     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+     \n
+     SELECT ?awardListName WHERE {
+        dbr:Lady_Gaga  dbp:name ?name .
+        dbr:Lady_Gaga  dbp:awards ?awardLink.
+        ?awardLink dbo:wikiPageWikiLink ?awardList.
+       ?awardList foaf:name ?awardListName
+
+        }
+        `
+       // Encodage de l'URL à transmettre à DBPedia
+     var url_base = "http://dbpedia.org/sparql";
+     var url = url_base + "?query=" + encodeURIComponent(contenu_requete) + "&format=json";
+ 
+     // Requête HTTP et affichage des résultats
+     var xmlhttp = new XMLHttpRequest();
+     xmlhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             var results = JSON.parse(this.responseText);
+             afficherListeResultats(results);
+         }
+}
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+};
+
+
+
 function getNumberOfSongs(name){
     var contenu_requete =     
     `PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
