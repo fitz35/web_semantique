efe47818248b2eac62f878aea6edb4bd048a9b61
Taha Mdarhri <t.mdarhri@gmail.com>
2021-12-06 10:26:27 +0100

    albumJScleancode

--- src/js/albums/albums.js
+++ src/js/albums/albums.js
@@ -1,21 +1,3 @@
-/*(function() {
-    document.getElementById("requete").innerHTML = `PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX dc: <http://purl.org/dc/elements/1.1/>
-PREFIX : <http://dbpedia.org/resource/>
-PREFIX dbpedia2: <http://dbpedia.org/property/>
-PREFIX dbpedia: <http://dbpedia.org/>
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-\n
-SELECT ?name ?album ?Songtitle ?SongLength WHERE {
-    ?album a dbo:Album; dbp:name ?name; dbo:wikiPageID ?id; dbp:title ?Songtitle.
-    FILTER(?id = 196945)
-    }`;
-})();*/
-
 function rechercher() {
     var contenu_requete = `PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
@@ -52,33 +34,10 @@ limit 1`;
   // Affichage des résultats dans un tableau
   function afficherResultats(data)
   {
-    // Tableau pour mémoriser l'ordre des variables ; sans doute pas nécessaire
-    // pour vos applications, c'est juste pour la démo sous forme de tableau
-    //var index = [];
-
-    //var contenuTableau = "<tr>";
     var name;
-
     data.results.bindings.forEach(r => {
-      
-      /*index.forEach(v => {
-
-        if (r[v].type === "uri")
-        {
-          contenuTableau += "<td><a href='" + r[v].value + "' target='_blank'>" + r[v].value + "</a></td>";
-        }
-        else {
-          contenuTableau += "<td>" + r[v].value + "</td>";
-        }
-      });*/
-
-
       name = r.name.value;
     });
 
-
-    //name = data.results.bindings[0].;
-
     document.getElementById("name").innerHTML = name;
-
   }
