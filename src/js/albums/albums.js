5c0e818d4235fbf7cda18aabd0f03eaaec03ac73
Taha Mdarhri <t.mdarhri@gmail.com>
2021-12-06 10:38:35 +0100

    AlbumJS desc and artist

--- src/js/albums/albums.js
+++ src/js/albums/albums.js
@@ -1,4 +1,15 @@
-function rechercher() {
+//Appel de toutes les fonctions de recherche
+function appel() {
+    rechercherNom();
+    rechercherDescription();
+    rechercherArtiste();
+    
+}
+//
+//
+//Recuperer le nom de l album
+//
+function rechercherNom() {
     var contenu_requete = `PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -24,7 +35,55 @@ limit 1`;
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var results = JSON.parse(this.responseText);
-            afficherResultats(results);
+            afficherResultatsName(results);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
+
+  // Affichage des résultats dans un tableau
+  function afficherResultatsName(data)
+  {
+    var name;
+    data.results.bindings.forEach(r => {
+      name = r.name.value;
+    });
+
+    document.getElementById("name").innerHTML = name;
+  }
+
+//
+//
+//Recuperer la description de l album
+//
+function rechercherDescription() {
+    var contenu_requete = `PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX : <http://dbpedia.org/resource/>
+PREFIX dbpedia2: <http://dbpedia.org/property/>
+PREFIX dbpedia: <http://dbpedia.org/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT ?desc WHERE {
+?album dbo:abstract ?desc; dbo:wikiPageID ?id.
+filter(?id = 7615743 && langMatches(lang(?desc),"EN"))
+}
+limit 1`;
+    // Encodage de l'URL à transmettre à DBPedia
+    var url_base = "http://dbpedia.org/sparql";
+    var url = url_base + "?query=" + encodeURIComponent(contenu_requete) + "&format=json";
+
+    // Requête HTTP et affichage des résultats
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var results = JSON.parse(this.responseText);
+            afficherResultatsDescription(results);
         }
     };
     xmlhttp.open("GET", url, true);
@@ -34,10 +93,57 @@ limit 1`;
   // Affichage des résultats dans un tableau
   function afficherResultats(data)
   {
-    var name;
+    var desc;
     data.results.bindings.forEach(r => {
-      name = r.name.value;
+      desc = r.desc.value;
     });
 
-    document.getElementById("name").innerHTML = name;
+    document.getElementById("description").innerHTML = desc;
+  }
+//
+//
+//Recuperer le nom de l artiste de l album
+//
+function rechercherArtiste() {
+    var contenu_requete = `PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX : <http://dbpedia.org/resource/>
+PREFIX dbpedia2: <http://dbpedia.org/property/>
+PREFIX dbpedia: <http://dbpedia.org/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT ?artist WHERE {
+?album dbp:artist ?artist; dbo:wikiPageID ?id.
+filter(?id = 7615743 && langMatches(lang(?artist),"EN"))
+}
+limit 1`;
+    // Encodage de l'URL à transmettre à DBPedia
+    var url_base = "http://dbpedia.org/sparql";
+    var url = url_base + "?query=" + encodeURIComponent(contenu_requete) + "&format=json";
+
+    // Requête HTTP et affichage des résultats
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var results = JSON.parse(this.responseText);
+            afficherResultatsArtiste(results);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
+
+  // Affichage des résultats dans un tableau
+  function afficherResultatsArtiste(data)
+  {
+    var artist;
+    data.results.bindings.forEach(r => {
+      artist = r.artist.value;
+    });
+
+    document.getElementById("artist").innerHTML = artist;
   }
