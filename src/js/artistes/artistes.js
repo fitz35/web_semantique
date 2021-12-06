75994ca8b40dc81982b3dc53487d65e208331d61
Tushita Ramkarn <tushitaramkaran@gmail.com>
2021-12-06 10:55:14 +0100

    add total number of songs

--- src/js/artistes/artistes.js
+++ src/js/artistes/artistes.js
@@ -86,12 +86,46 @@ function getArtistSongs(name){
              afficherListeResultats(results);
          }
 }
-
-
-
-xmlhttp.open("GET", url, true);
+    xmlhttp.open("GET", url, true);
     xmlhttp.send();
 };
+
+
+function getNumberOfSongs(name){
+    var contenu_requete =     
+    `PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+     PREFIX dc: <http://purl.org/dc/elements/1.1/>
+     PREFIX : <http://dbpedia.org/resource/>
+     PREFIX dbpedia2: <http://dbpedia.org/property/>
+     PREFIX dbpedia: <http://dbpedia.org/>
+     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+     \n
+     SELECT (COUNT (?songName) AS ?total) WHERE {
+        dbr:${name}  dbp:name ?name .
+        ?song dbo:artist dbr:${name}.
+        ?song gold:hypernym dbr:Song.
+        ?song dbp:name ?songName.
+        }
+        `
+       // Encodage de l'URL à transmettre à DBPedia
+     var url_base = "http://dbpedia.org/sparql";
+     var url = url_base + "?query=" + encodeURIComponent(contenu_requete) + "&format=json";
+ 
+     // Requête HTTP et affichage des résultats
+     var xmlhttp = new XMLHttpRequest();
+     xmlhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             var results = JSON.parse(this.responseText);
+             afficherTotal(results);
+         }
+}
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+};
+
 
 
 function afficherListeResultats(data){
@@ -102,7 +136,7 @@ function afficherListeResultats(data){
             var resultTableList=document.getElementsByClassName("titleSing");
             for(let parcours of resultTableList)
             {
-                parcours.innerHTML+= `<li> <a href="${v.song.value}">${v.songName.value}</a></li>`;
+                parcours.innerHTML+=`<li> <a href="${v.song.value}">${v.songName.value}</a></li>`;
                 
             }
         
@@ -162,6 +196,18 @@ function afficherResultats(data){
         
       }
 )};
+
+function afficherTotal(data){
+    console.log(data);
+    data.results.bindings.forEach((v, i) => {
+        
+            var resutTable=document.getElementsByClassName("total");
+            for(let parcours of resutTable)
+            {
+                parcours.innerHTML=v.total.value;
+            }
+        }
+    )};
 
    
     
